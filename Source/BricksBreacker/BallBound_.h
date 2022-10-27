361c036b04903892b962a4d3d52ebafbef08da88
cabarc750 <cabarc750@gmail.com>
2022-10-26 22:45:29 -0400

    Lo propio.

---
+++ Source/BricksBreacker/BallBound_.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Ball.h"
+#include "BallBound_.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BRICKSBREACKER_API ABallBound_ : public ABall
+{
+	GENERATED_BODY()
+	
+};
